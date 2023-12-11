7a8fee09a0567a0f278c798c22bd21a4a8368229
DE Stuff <91672788+KiranGunturu@users.noreply.github.com>
2023-12-11 18:26:48 +0000

    larger files removed

---
+++ terraform-project/module/ec2_instance/variables.tf
@@ -0,0 +1,13 @@
+variable "ami_value" {
+    description = "value for the AMI"
+}
+
+variable "instance_type_value" {
+    description = "value for the instance_type"
+  
+}
+
+variable "subnet_id_value" {
+    description = "value of the subnet_id"
+  
+}
